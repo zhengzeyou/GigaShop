397f3fceff376947f433b4f1a19aced8f1c4f9ad
dlwpdlr <zzoon@jakeui-macbook-pro.local>
2018-11-10 17:04:07 +0800

    ggg

---
+++ GigaShop/Home/HomeSubviews/GSHomeTopView.swift
@@ -0,0 +1,30 @@
+//
+//  GSHomeTopView.swift
+//  GigaShop
+//
+//  Created by dlwpdlr on 2018/11/10.
+//  Copyright © 2018年 GIGA Korea. All rights reserved.
+//
+
+import UIKit
+
+class GSHomeTopView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.backgroundColor = Constant.redColor
+		addSuv()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+}
+
+
+extension GSHomeTopView {
+	fileprivate func addSuv(){
+		
+	}
+
+}
